a789bb542635feceb7968ae7475cabf165bd5bf8
Berk Yavuz <berkyavuz57@gmail.com>
2026-01-05 02:18:59 +0300

    hesapmakinesi.c

--- hesapmakinesi.c
+++ hesapmakinesi.c
@@ -4,15 +4,12 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	
 	float sayi1,sayi2,sonuc;
 	char islem;
-	
 	printf("hangi islemi yapmak istiyorsunuz seciniz(-,+,/,*): "); 
 	scanf("%c",&islem);
 	printf("2 adet sayi giriniz: ");
 	scanf("%f%f",&sayi1,&sayi2);
-	
 	switch(islem){
 		case '-':
 		{
@@ -39,9 +36,7 @@ int main(int argc, char *argv[]) {
 			break;
 		}
 		default:
-			printf("girdiginiz secim geçerli degil lutfen gecerli bir secim yapiniz !HATA42z4j98");
-			
+			printf("girdiginiz secim geÃ§erli degil lutfen gecerli bir secim yapiniz !HATA42z4j98");		
 	}
-	
 	return 0;
 }
