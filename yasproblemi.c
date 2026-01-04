d9b7637ac1f6da447a781908c49138dcc5be70ee
Berk Yavuz <berkyavuz57@gmail.com>
2026-01-05 02:18:07 +0300

    yasproblemi.c

--- yasproblemi.c
+++ yasproblemi.c
@@ -5,32 +5,25 @@
 
 int main(int argc, char *argv[]) 
 {
-	 int yas,sayac;
+	int yas,sayac;
     sayac=0;
-    
     while(sayac<5)
     {
-        
 		printf("Yas giriniz:");
 		scanf("%d",&yas);
-        
         if(yas>0)
         {
-        	
-		
 			if(yas<18)
             	printf("Cocuksunuz.\n");
         	else if (yas>18&&yas<45)
             	printf("Gencsiniz.\n");
         	else
             	printf("Yaslisiniz.\n");
-
         	sayac++;
         }
         else
         	printf("yas degeri 0 dan kucuk olamaz\n");
     }
-
     getch();
     return 0;
 } 
